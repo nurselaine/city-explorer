223c0698d24073a89bbcc4af9b0c429d3c8ed8c0
nurselaine <Elaine.t.huynh@gmail.com>
2022-06-20 20:07:41 -0700

    added css and display a map using retrieved data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React from 'react';
 import './App.css';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Figure from 'react-bootstrap/Figure';
 
 class App extends React.Component {
   constructor(props){
@@ -10,6 +11,9 @@ class App extends React.Component {
       userInput: '',
       city: '',
       cityObj: {},
+      lat: 0,
+      lon: 0,
+      cityImg: '',
     }
   }
   handleChange = (e) => {
@@ -28,22 +32,36 @@ class App extends React.Component {
     let cityInfo = await axios.get(url);
     this.setState({
       cityObj: cityInfo.data[0],
+      lat: this.state.cityObj.lat,
+      lon: this.state.cityObj.lon,
+      
     })
   }
 
   render() {
     return (
-      <div>
+      <div className="app-container">
         <form onSubmit={this.handleSubmit}>
-          <label>Enter City Name:</label>
-          <input value={this.state.userInput} onChange={this.handleChange}/>
+          <label>City Explorer</label>
+          <input placeholder="Enter A City 🔎" value={this.state.userInput} onChange={this.handleChange}/>
           <button type='submit'>Explore!</button>
         </form>
-        <ListGroup variant="flush">
-          <ListGroup.Item>{this.state.cityObj.display_name}</ListGroup.Item>
-          <ListGroup.Item>Latitude: {this.state.cityObj.lat}</ListGroup.Item>
-          <ListGroup.Item>Longitude: {this.state.cityObj.lon}</ListGroup.Item>
-        </ListGroup>
+        <Figure>
+      <Figure.Image
+        width={371}
+        height={380}
+        alt={this.state.cityObj.display_name}
+        src={this.state.cityObj.display_name ? `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.cityObj.lat},${this.state.cityObj.lon}&zoom=12` 
+            : '../logo192.png'}
+      />
+        <Figure.Caption>
+          <ListGroup variant="flush">
+            <ListGroup.Item>{this.state.cityObj.display_name}</ListGroup.Item>
+            <ListGroup.Item>Latitude: {this.state.cityObj.lat}</ListGroup.Item>
+            <ListGroup.Item>Longitude: {this.state.cityObj.lon}</ListGroup.Item>
+          </ListGroup>
+        </Figure.Caption>
+      </Figure>
       </div>
     )
   }
