77692d5c16ed9408f33e49fa573c873a8c609105
nurselaine <Elaine.t.huynh@gmail.com>
2022-06-22 22:22:31 -0700

    added handler for movie data and carousel component to display movies

---
+++ src/components/Movies.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+
+class Movies extends React.Component{
+  render() {
+    console.log(this.props.data);
+    console.log(this.props.data[0]);
+    let movie = this.props.data.map((obj, idx) => {
+      return (
+        <Carousel.Item>
+          <img 
+            className='d-block w-100'
+            src={`.${obj.imgUrl}`}
+            alt={idx}
+          />
+          <Carousel.Caption>
+            <h3>{obj.title}</h3>
+            <p>{obj.overview}</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      )
+    })
+    return (
+      <Carousel>
+        {movie}
+      </Carousel>
+    );
+  }
+}
+export default Movies;
